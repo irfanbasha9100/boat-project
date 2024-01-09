1387dd744a12308633c02c3609e6f0c4374b96e9
irfanbasha9100 <irfanbashaparigi9133@gmail.com>
2024-01-09 09:58:10 +0530

    Banner

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,20 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png"
+
 const Banner = () => {
-    return <div>Banner</div>;
+    return <div className="hero-banner">
+        <div className="content">
+            <div className="text-content">
+                <h1>SALES</h1>
+                <p> Enjoy the Vibe With the Best Wireless Headphones. Shop Now at boAt Official. Get yourself immersed in the colors of musical bliss by choosing boAt Rockerz</p>
+                <div className="ctas">
+                    <div className="banner-cta">Read More</div>
+                    <div className="banner-cta v2">Banner Cta</div>
+                </div>
+            </div>
+            <img src={BannerImg} alt="Banner Image" />
+        </div>
+    </div>;
 };
 
 export default Banner;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.scss";
+import Banner from './Banner/Banner'
 const Home = () => {
-    return <div className="home">Home</div>;
+    return <div className="home"><Banner/></div>;
 };
 
 export default Home;
