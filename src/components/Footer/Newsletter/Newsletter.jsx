4fdb8f7350e1afb239c9c24f6073dcc2ccdc8632
irfanbasha9100 <irfanbashaparigi9133@gmail.com>
2024-01-11 08:54:19 +0530

    Banner completed

--- src/components/Footer/Newsletter/Newsletter.jsx
+++ src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,12 @@
 import "./Newsletter.scss";
+import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+    return <div></div>;
 };
 
 export default Newsletter;
--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,20 +1,25 @@
+import React from "react";
+
 import "./Banner.scss";
-import BannerImg from "../../../assets/banner-img.png"
+import BannerImg from "../../../assets/banner-img.png";
 
 const Banner = () => {
-    return <div className="hero-banner">
-        <div className="content">
-            <div className="text-conte nt">
-                <h1>SALES</h1>
-                <p> Enjoy the Vibe With the Best Wireless Headphones. Shop Now at boAt Official. Get yourself immersed in the colors of musical bliss by choosing boAt Rockerz</p>
-                <div className="ctas">
-                    <div className="banner-cta">Read More</div>
-                    <div className="banner-cta v2">Banner Cta</div>
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>SALES</h1>
+                    <p>From deep, rich bass to clean highs, you’ll hear every note with a new sense of clarity. boAt wireless earphones can last for hours with boAt’s ASAP™ Fast Charge technology
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
                 </div>
+                <img className="banner-img" src={BannerImg} alt="Home" />
             </div>
-            <img src={BannerImg} alt="Banner Image" />
         </div>
-    </div>;
+    );
 };
 
 export default Banner;
