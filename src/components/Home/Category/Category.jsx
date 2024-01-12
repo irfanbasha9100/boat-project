b3c4ba29cb598214f383ac0fb2f736b871edb557
irfanbasha9100 <irfanbashaparigi9133@gmail.com>
2024-01-12 11:58:03 +0530

    Products Page

--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -1,16 +1,9 @@
-import "./Category.scss";
-import Products from "../../Products/Products"
-const Category = () => {
-    return (
-        <div className="category-main-content">
-            <div className="layout">
-                <div className="category-title">
-                    Category Title
-                </div>
-                <Products innerPage={true}/>
-            </div>
-        </div>
-    );
-};
+import React from 'react'
 
-export default Category;
+const Category = () => {
+  return (
+    <div>Category</div>
+  )
+}
+
+export default Category
